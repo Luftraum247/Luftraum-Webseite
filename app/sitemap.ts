27cbb64c93c2d1b07ac27ecb4a534912dcb7241a
Luftraum247 <155480886+Luftraum247@users.noreply.github.com>
2026-09-25 00:04:49 +0200

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -17,6 +17,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: `${baseUrl}/ueber-uns`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
       url: `${baseUrl}/kontakt`,
       lastModified: new Date(),
       changeFrequency: "monthly",
